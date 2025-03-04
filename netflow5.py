a2bc135db7581114628bf8f600186dd4026fcb14
PrawnMadeInUA <prawnua@gmail.com>
2025-03-04 11:02:47 +0200

    Update netflow5.py

--- netflow5.py
+++ netflow5.py
@@ -11,16 +11,17 @@ try:
             SETTINGS[key] = value
 except FileNotFoundError:
     print("File settings.txt not found in the same folder")
-    exit
+    exit()
 
 #Settings
 SERVER_IP = SETTINGS["SERVER_IP"]
 SERVER_PORT = int(SETTINGS["SERVER_PORT"])
+print(type(SERVER_PORT))
 
 #Server for listening netflow v5 traffic
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((SERVER_IP, SERVER_PORT))
-print("Listening for NetFlow on {SERVER_IP}:{SERVER_PORT}")
+print(f"Listening for NetFlow on {SERVER_IP}:{SERVER_PORT}")
 
 print(sock)
 
